1ab8bb33ed615b13dca78418c7eb9e1533d9360f
Charly Lima <charlylima@github>
2023-10-21 09:47:04 +0200

    add test.sh script

---
+++ test.sh
@@ -0,0 +1,10 @@
+# First test with this command:
+plasmoidviewer -a .
+
+# If you have it installed in ~/.local/share/plasma/plasmoids/com.github.charlylima.webcamwidget/, then you can test it with this command:
+#plasmawindowed com.github.charlylima.webcamwidget
+
+# Real test: 
+#plasmashell --replace
+
+# See https://develop.kde.org/docs/plasma/widget/testing/
